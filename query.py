24cc169cc4318219cb539ec23c5f0bdbfdbf2ff3
msyed96 <maheen_syed@brown.edu>
2019-11-25 00:11:14 +0000

    lab

--- query.py
+++ query.py
@@ -8,7 +8,7 @@ db = MongoClient().test
 
 
 def query1(minFare, maxFare):
-    """ Finds taxi rides with fare amount greater than or equal to minFare and less than or equal to maxFare.  
+    """ Finds taxi rides with fare amount greater than or equal to minFare and less than or equal to maxFare.
 
     Args:
         minFare: An int represeting the minimum fare
@@ -23,7 +23,14 @@ def query1(minFare, maxFare):
         An array of documents.
     """
     docs = db.taxi.find(
-        # TODO: implement me
+        {
+            "fare_amount": {"$gte": minFare, "$lte": maxFare}
+        },
+        {
+            "_id": 0,
+            "pickup_longitude": 1,
+            "pickup_latitude": 1,
+        }
     )
 
     result = [doc for doc in docs]
@@ -31,7 +38,7 @@ def query1(minFare, maxFare):
 
 
 def query2(textSearch, minReviews):
-    """ Finds airbnbs with that match textSearch and have number of reviews greater than or equal to minReviews.  
+    """ Finds airbnbs with that match textSearch and have number of reviews greater than or equal to minReviews.
 
     Args:
         textSearch: A str representing an arbitrary text search
@@ -71,21 +78,20 @@ def query2(textSearch, minReviews):
 
 
 def query3():
-    """ Groups airbnbs by neighbourhood_group and finds average price of each neighborhood_group sorted in descending order.  
+    """ Groups airbnbs by neighbourhood_group and finds average price of each neighborhood_group sorted in descending order.
 
     Returns:
         An array of documents.
     """
     docs = db.airbnb.aggregate(
-        # TODO: implement me
-    )
+        [{"$group": {"_id": "$neighbourhood_group", "total": {"$avg": "$price"}}},
+            {"$sort": {"total": -1}}])
 
     result = [doc for doc in docs]
     return result
 
-
 def query4():
-    """ Groups taxis by pickup hour. 
+    """ Groups taxis by pickup hour.
         Find average fare for each hour.
         Find average manhattan distance travelled for each hour.
         Count total number of rides per pickup hour.
@@ -95,22 +101,33 @@ def query4():
         An array of documents.
     """
     docs = db.taxi.aggregate(
-        # TODO: implement me
-    )
+            [{"$group":
+              {"_id": {"$hour": "$pickup_datetime"},
+               "avgfare": {"$avg": "$fare_amount"},
+               "distance":
+               {"$avg":
+                {"$add":
+                 [{"$abs":
+                   {"$subtract":
+                    ["$pickup_longitude", "$dropoff_latitude"]}},
+                  {"$abs":
+                   {"$subtract":
+                    ["$pickup_latitude", "$dropoff_latitude"]}}]}},
+               "count": {"$sum": 1}}},
+             {"$sort": {"avgfare": -1}}])
     result = [doc for doc in docs]
     return result
 
 
-def query5():
-    """ Finds airbnbs within 1000 meters from location (longitude, latitude) using geoNear. 
-        Find average fare for each hour.
-        Find average manhattan distance travelled for each hour.
-        Count total number of rides per pickup hour.
-        Sort by average fare in descending order.
+def query5(latitude, longitude):
+    """ Finds airbnbs within 1000 meters from location (longitude, latitude) using geoNear.
+
+    Args:
+        latitude: A float representing latitude coordinate
+        longitude: A float represeting longitude coordinate
 
     Projection:
         dist
-        location
         name
         neighbourhood
         neighbourhood_group
@@ -119,8 +136,35 @@ def query5():
 
 
     """
-    docs = db.airbnb.aggregate(
-        # TODO: implement me
-    )
+    docs = db.airbnb.aggregate([
+        {
+            '$geoNear': {
+                'near': {'type': 'Point', 'coordinates': [longitude, latitude]},
+                'distanceField': 'dist.calculated',
+                'maxDistance': 1000,
+                'spherical': False
+            }
+        },
+        {
+            '$project': {
+                '_id': 0,
+                'dist': 1,
+                'name': 1,
+                'neighbourhood': 1,
+                'neighbourhood_group': 1,
+                'price': 1,
+                'room_type': 1
+            }
+        },
+        {
+            '$sort': {'dist': 1}
+        }
+    ])
     result = [doc for doc in docs]
     return result
+
+
+if __name__ == "__main__":
+    #print(query1(20, 123))
+    #print(query3())
+    print(query4())
